b34d79d9d0fd3636302d30f0c172a1e7a2a65154
OUghTMS <misha.saan@gmail.com>
2019-11-25 23:40:01 +0300

    Updates

---
+++ precache-manifest.dfb7f40fe6a4eb495cd10ef9f0a40e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee9b6bd0bb0794e6cfaeff3956e64a9",
+    "url": "/project-canvas/index.html"
+  },
+  {
+    "revision": "ece0022c1799f1842538",
+    "url": "/project-canvas/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9f2857e0c6b0b16956ae",
+    "url": "/project-canvas/static/js/2.ba8ae772.chunk.js"
+  },
+  {
+    "revision": "ece0022c1799f1842538",
+    "url": "/project-canvas/static/js/main.24b83f91.chunk.js"
+  },
+  {
+    "revision": "f206812912f0ac9600c3",
+    "url": "/project-canvas/static/js/runtime-main.bc516af6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/project-canvas/static/media/logo.25bf045c.svg"
+  }
+]);
