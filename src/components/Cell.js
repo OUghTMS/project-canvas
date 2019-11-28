2ecfac5abf4e15b3811bff6fc4de06e060aa2399
OUghTMS <misha.saan@gmail.com>
2019-11-28 12:33:53 +0300

    feat: add new style for cell component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 export default function Cell(props) {
+    
+    const styleClass = () => {
+        const cell = 'cell';
+
+        if (props.cell === 'x') {
+          
+            return `${cell} blueviolet-cell`
+        } else if (props.cell === 'o') {
+            
+            return  `${cell} green-cell`
+        } else {
+
+            return cell;
+        }
+    } 
     return (
-        <div className='cell'>
-            {props.cell}
+        <div className={styleClass()}>
         </div>
     );
 }
