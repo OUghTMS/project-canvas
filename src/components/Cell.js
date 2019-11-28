a4d1efd553b534b79783ba8f1d2acfb91ba082bc
OUghTMS <misha.saan@gmail.com>
2019-11-28 18:16:59 +0300

    feat: add any type of cell color

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,23 +1,33 @@
 import React from "react";
 
 export default function Cell(props) {
-    
-    const styleClass = () => {
-        const cell = 'cell';
 
-        if (props.cell === 'x') {
-          
-            return `${cell} blueviolet-cell`
-        } else if (props.cell === 'o') {
-            
-            return  `${cell} green-cell`
-        } else {
+    const cellBackground = () => {
 
-            return cell;
+        const s = new Option().style;
+        s.color = props.cell;
+        
+        if ( s.color !== '' ) {
+
+            return props.cell;
         }
-    } 
+
+        const colors = props.cell.split('').map((char) => '00'.concat(char.charCodeAt(0).toString(2)).slice(-6));
+        const colorsRGB = colors.map( color => color.match(/.{1,2}/g) );
+
+        let RGB = Array(3);
+        for(let i = 0; i < 3; i++){
+
+            RGB[i] = colorsRGB.reduce( (acc, value) => acc + parseInt(value[i], 2), 0 );
+        }
+        const maxRGB = Math.max(...RGB);
+        RGB = RGB.map( color => color/maxRGB * 255 );
+
+        return `rgb(${RGB[0]}, ${RGB[1]}, ${RGB[2]})`;
+    }
+
     return (
-        <div className={styleClass()}>
+        <div className="cell" style={{ background: cellBackground() }}>
         </div>
     );
 }
